07f29e1dff1dcc9187a4dde4c74f82b5d80737e0
Hima <84233494+Himatric@users.noreply.github.com>
2022-01-13 19:31:27 +0100

    Update console.py

--- semid/util/console.py
+++ semid/util/console.py
@@ -1,4 +1,4 @@
-import ctypes, textwrap
+import ctypes, textwrap, os
 class Console:
     @staticmethod
     def color(text) -> str:
@@ -11,7 +11,10 @@ class Console:
     def set_title(title:str) -> bool:
         import sys
         sys.stdout.flush()
-        return ctypes.windll.kernel32.SetConsoleTitleA(title.encode())
+        if os.name == "nt":
+            return ctypes.windll.kernel32.SetConsoleTitleA(title.encode())
+        else:
+            return print("\x1b]0;"+title.encode(), end="\a")
     def center(text:str) -> str:
         lines = textwrap.wrap(text)
         return "\n".join(line.center(100) for line in lines)
@@ -19,6 +22,3 @@ class Console:
         return f"\033[31m{text}\033[0m"
     def green(text:str) -> str:
         return f"\033[32m{text}\033[0m"
-
-
-    
